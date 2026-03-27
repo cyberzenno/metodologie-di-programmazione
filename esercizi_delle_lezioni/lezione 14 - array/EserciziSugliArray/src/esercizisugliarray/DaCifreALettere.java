b0728799dbf53dca7a01fbe825d0c05a8907169f
Zenno <zenno.neagu@gmail.com>
2026-03-27 17:40:49 +0100

    da cifre a lettere

---
+++ esercizi_delle_lezioni/lezione 14 - array/EserciziSugliArray/src/esercizisugliarray/DaCifreALettere.java
@@ -0,0 +1,25 @@
+package esercizisugliarray;
+
+public class DaCifreALettere {
+
+	public static String daCifreALettere(String cifre) {
+		String[] lettere = new String[] { "zero", "uno", "due", "tre", "quattro", "cinque", "sei", "sette", "otto",
+				"nove" };
+
+		String cifreComeLettere = "";
+
+		for (int i = 0; i < cifre.length(); i++) {
+			int cifra = Integer.parseInt(cifre.charAt(i) + "");
+
+			cifreComeLettere += (cifreComeLettere.isEmpty() ? "" : " ") + lettere[cifra];
+		}
+
+		return cifreComeLettere;
+	}
+
+	public static void main(String... args) {
+		var lettere = DaCifreALettere.daCifreALettere("8495648");
+
+		System.out.println(lettere);
+	}
+}
