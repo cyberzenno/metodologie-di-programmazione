5909c905a789684b50bc6dc8105ec96b3b57e53a
Zenno <zenno.neagu@gmail.com>
2026-04-03 11:44:49 +0200

    tavola pitagoriga

--- esercizi_delle_lezioni/lezione 14 - array/EserciziSugliArray/src/esercizisullematrici/TavolaPitagorica.java
+++ esercizi_delle_lezioni/lezione 14 - array/EserciziSugliArray/src/esercizisullematrici/TavolaPitagorica.java
@@ -16,24 +16,31 @@ public class TavolaPitagorica {
 
 	public void stampaConIntestazioni() {
 
+		// aggiungiamo la prima riga come intestazione
+		for (int j = 0; j < tavola[0].length; j++) {
+
+			if (j == 0)
+				System.out.printf("%3s |", "x");
+
+			System.out.printf("%3d ", tavola[1][j]);
+		}
+
+		System.out.println();
+		stampaRigaDiLiniette();
+		System.out.println();
+
 		for (int i = 0; i < tavola.length; i++) {
 			for (int j = 0; j < tavola[0].length; j++) {
 
-				// qua e' un po' un casotto
-				// to be continued...
+				// aggiungiamo il primo numero come intestazione riga
+				if (j == 0)
+					System.out.printf("%3s |", i);
 
 				System.out.printf("%3d ", tavola[i][j]);
 			}
 
 			System.out.println();
 		}
-
-	}
-
-	public static void main(String[] args) {
-		TavolaPitagorica t = new TavolaPitagorica(11);
-
-		t.stampaConIntestazioni();
 	}
 
 	private void popolaTavola() {
@@ -42,5 +49,21 @@ public class TavolaPitagorica {
 				tavola[i][j] = i * j;
 			}
 		}
+	}
+
+	private void stampaRigaDiLiniette() {
+		for (int i = 0; i < tavola.length + 1; i++) {
+			System.out.printf("%3s ", "-");
+		}
+	}
+
+	public static void main(String[] args) {
+		TavolaPitagorica t = new TavolaPitagorica(11);
+
+		t.stampaConIntestazioni();
+
+		//funziona anche get prodotto, ma andiamo avanti
+		//var prodotto = t.getProdotto(8, 9);
+		//System.out.println(prodotto);
 	}
 }
